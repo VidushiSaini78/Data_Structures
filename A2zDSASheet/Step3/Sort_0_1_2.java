82ca4b3f38d97b00a21fa7ef409b34f5a8d3d340
Vidushi Saini <112749021+VidushiSa01@users.noreply.github.com>
2024-04-16 16:21:25 +0530

    Create Sort_0_1_2.java

---
+++ A2zDSASheet/Step3/Sort_0_1_2.java
@@ -0,0 +1,54 @@
+/*
+Given an array containing 0 , 1 and 2
+Brute Force : - Sorting technique (NlogN)
+Better Approach : Count no of 0 ,1 and 2 TC:- o(N) + o(N)
+Optimized Appraoch :- TC : O(N)
+*/
+import java.util.Scanner;
+public class practise
+{
+    static  Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        int size = input.nextInt();
+        int ar[] = new int[size];
+        input(ar);
+        sort(ar);
+    }
+    static void input(int ar[] )
+    {
+        for(int i=0 ; i<ar.length ; i++)
+        {
+            ar[i] = input.nextInt();
+        }
+    }
+    static void sort(int ar[])
+    {
+        int Low = 0 ;
+        int Mid = 0;
+        int High = ar.length-1;
+        while(Mid <= High)
+        {
+            int value =  ar[Mid];
+            if(value==1)
+                Mid++;
+            else if(value ==0)
+            {
+                ar[Mid] = ar[Low];
+                ar[Low]=value;
+                Low++;
+                Mid++;
+            }
+            else
+            {
+                ar[Mid] = ar[High];
+                ar[High] = value;
+                High--;
+
+            }
+        }
+        for(int i=0; i<ar.length ; i++)
+        {
+            System.out.print(ar[i] + " ");
+        }
+    }
+}
