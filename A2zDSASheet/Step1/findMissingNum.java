c1d9d04784fb7fc8c6bdbbbfc83d93835dfe4e23
Vidushi Saini <112749021+VidushiSa01@users.noreply.github.com>
2024-05-28 08:58:20 +0530

    Update findMissingNum.java
    added question explanation

--- A2zDSASheet/Step1/findMissingNum.java
+++ A2zDSASheet/Step1/findMissingNum.java
@@ -1,3 +1,11 @@
+/*
+Problem Statement: Given an integer N and an array of size N-1 containing N-1 numbers between 1 to N. 
+Find the number(between 1 to N), that is not present in the given array.
+Input Format:
+ N = 5, array[] = {1,2,4,5}
+Result:
+ 3
+*/
 import java.util.*;
 public class findMissingNum
 {
