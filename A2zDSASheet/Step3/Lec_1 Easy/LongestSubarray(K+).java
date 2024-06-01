f5d8e3c4a47c91f01cc8dd001c90cd921e05758e
Vidushi Saini <112749021+VidushiSa01@users.noreply.github.com>
2024-06-01 23:42:19 +0530

    Update LongestSubarray(K+).java
    added the optimal code

--- A2zDSASheet/Step3/Lec_1 Easy/LongestSubarray(K+).java
+++ A2zDSASheet/Step3/Lec_1 Easy/LongestSubarray(K+).java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 public class Solution {
+    // Solution 3 applicable to both types of arrays 1) containing only positive 2) Containing +ve or -ve
     public static int longestSubarrayWithSumK(int[] ar, long k) {
         HashMap<Long, Integer> mapp = new HashMap<>();
         Long prefixSum =0L;
@@ -20,6 +21,24 @@ public class Solution {
             {
                 mapp.put(prefixSum , i);
             }
+        }
+        return maxLength;
+    }
+    // Solution 4 most optimized (Only valid for array having only positive number)
+    static int longest(int ar[], int k) {
+        int maxLength = -1;
+        int start = 0;
+        int end = 0;
+        int current_sum = 0;
+        while (end < ar.length) {
+            current_sum += ar[end];
+            if (current_sum == k)
+                maxLength = Math.max(maxLength, end - start + 1);
+            else if (current_sum > k) {
+                current_sum -= ar[start];
+                start++;
+            }
+            end++;
         }
         return maxLength;
     }
