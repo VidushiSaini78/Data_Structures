b3a0301d99638930b1f082c7f22b02a923931615
Vidushi Saini <112749021+VidushiSa01@users.noreply.github.com>
2024-07-13 12:54:16 +0530

    Create Reverse_Pairs.java
    leetcode - 493

---
+++ A2zDSASheet/Step3/Lec_3 Hard/Reverse_Pairs.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+class Solution {
+
+    public int reversePairs(int[] arr) {
+        return mergeSort(arr, 0, arr.length - 1);
+    }
+
+    public static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] > (2L * arr[right])) right++;
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+
+    public static int mergeSort(int[] arr, int low, int high) {
+        int cnt = 0;
+        if (low >= high) return cnt;
+        int mid = (low + high) / 2;
+        cnt += mergeSort(arr, low, mid);  // left half
+        cnt += mergeSort(arr, mid + 1, high); // right half
+        cnt += countPairs(arr, low, mid, high); // Modification
+        merge(arr, low, mid, high);  // merging sorted halves
+        return cnt;
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left]);
+                left++;
+            } else {
+                temp.add(arr[right]);
+                right++;
+            }
+        }
+
+        while (left <= mid) {
+            temp.add(arr[left]);
+            left++;
+        }
+
+        while (right <= high) {
+            temp.add(arr[right]);
+            right++;
+        }
+
+        for (int i = low; i <= high; i++) {
+            arr[i] = temp.get(i - low);
+        }
+    }
+}
