e980a3af1e84270d52aaf4cd4f63a373d5e86c54
VidushiSaini78 <124977033+VidushiSaini78@users.noreply.github.com>
2024-07-14 12:47:27 +0530

    Create Upper_And_Lower_Bound.java

---
+++ A2zDSASheet/Step4/Upper_And_Lower_Bound.java
@@ -0,0 +1,43 @@
+public class practise
+{
+    public static void main(String[] args) {
+        int ar[] = {1,2,3,3,5,8,9,11};
+        System.out.println(upper_bound(ar , 9));
+    }
+    static int upper_bound(int ar[] , int x)
+    {
+        int low = 0 ;
+        int ans = -1;
+        int high =  ar.length-1;
+        while(low <= high)
+        {
+            int mid = (low+high)/2;
+            if(ar[mid] > x)
+            {
+                ans = ar[mid];
+                high = mid-1;
+            } 
+            else low = mid+1;   
+
+        }
+    return ans;
+    }
+    static int lower_bound(int ar[] , int x)
+    {
+        int low = 0 ;
+        int ans = -1;
+        int high =  ar.length-1;
+        while(low <= high)
+        {
+            int mid = (low+high)/2;
+            if(ar[mid] >= x)
+            {
+                ans = ar[mid];
+                high = mid-1;
+            } 
+            else low = mid+1;   
+
+        }
+    return ans;
+    }
+}
